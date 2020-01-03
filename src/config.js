0ea0c2bc51fa36b1749c255c5208b0abbb059531
Valioesi <Vali_Schagerl@web.de>
2020-01-03 12:24:26 +0100

    changed pool ids and urls

--- src/config.js
+++ src/config.js
@@ -3,12 +3,15 @@ export default {
   API: {
     INVOKE_URL:
       process.env.NODE_ENV === 'development'
-        ? 'https://vmhbaao23c.execute-api.eu-central-1.amazonaws.com/dev'
-        : 'https://vmhbaao23c.execute-api.eu-central-1.amazonaws.com/prod',
+        ? 'https://ftwey058f0.execute-api.eu-central-1.amazonaws.com/dev'
+        : ' https://9jvbvaehkd.execute-api.eu-central-1.amazonaws.com/prod',
   },
   COGNITO: {
     REGION: 'eu-central-1',
-    USER_POOL_ID: 'eu-central-1_u8qvswCdL',
+    USER_POOL_ID:
+      process.env.NODE_ENV === 'development'
+        ? 'eu-central-1_C37zRqIwu'
+        : 'eu-central-1_bKQCTFOsz',
     USER_POOL_CLIENT_ID: process.env.REACT_APP_COGNITO_CLIENT_ID,
   },
 };
