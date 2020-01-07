827bee2b56614e0f37387817524f3669e3d40c9d
Valioesi <Vali_Schagerl@web.de>
2020-01-07 09:50:05 +0100

    merge conflicts

--- src/config.js
+++ src/config.js
@@ -3,15 +3,15 @@ export default {
   API: {
     INVOKE_URL:
       process.env.NODE_ENV === 'development'
-        ? 'https://ftwey058f0.execute-api.eu-central-1.amazonaws.com/dev'
-        : 'https://9jvbvaehkd.execute-api.eu-central-1.amazonaws.com/prod',
+        ? 'https://2j0bcp5tr9.execute-api.eu-central-1.amazonaws.com/dev'
+        : 'https://ag5gu1z06h.execute-api.eu-central-1.amazonaws.com/prod',
   },
   COGNITO: {
     REGION: 'eu-central-1',
     USER_POOL_ID:
       process.env.NODE_ENV === 'development'
-        ? 'eu-central-1_C37zRqIwu'
-        : 'eu-central-1_bKQCTFOsz',
+        ? 'eu-central-1_R7ucfhvJM'
+        : 'eu-central-1_q89dI31Fh',
     USER_POOL_CLIENT_ID: process.env.REACT_APP_COGNITO_CLIENT_ID,
   },
 };
