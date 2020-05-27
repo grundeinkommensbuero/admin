8a401cb10182a8e381591fd3f9598d4c93dcaa3b
Valioesi <Vali_Schagerl@web.de>
2020-05-27 18:16:11 +0200

    Added table with signature history

---
+++ src/components/Stats/SignatureStats/SignatureCountTable/index.js
@@ -0,0 +1,35 @@
+import { Table } from 'semantic-ui-react';
+import React from 'react';
+
+const SignatureCountTable = ({ stats }) => {
+  return (
+    <Table celled definition>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Kampagne</Table.HeaderCell>
+          <Table.HeaderCell>Inklusive gemischte Ämter</Table.HeaderCell>
+          <Table.HeaderCell>Exklusive gemischte Ämter</Table.HeaderCell>
+          <Table.HeaderCell>Von User*in gescannt</Table.HeaderCell>
+          <Table.HeaderCell>
+            Berechnet aus angekommen und gescannt
+          </Table.HeaderCell>
+          <Table.HeaderCell>Ab 27.02 angekommen</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {Object.keys(stats).map((campaign, index) => (
+          <Table.Row key={index}>
+            <Table.Cell>{campaign}</Table.Cell>
+            <Table.Cell>{stats[campaign].withMixed}</Table.Cell>
+            <Table.Cell>{stats[campaign].withoutMixed}</Table.Cell>
+            <Table.Cell>{stats[campaign].scannedByUser}</Table.Cell>
+            <Table.Cell>{stats[campaign].computed}</Table.Cell>
+            <Table.Cell>{stats[campaign].from27}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+};
+
+export default SignatureCountTable;
