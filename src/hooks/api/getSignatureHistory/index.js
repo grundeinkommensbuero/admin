e73c7025e4f1165f3e0a3cf6d3729076ba19a848
Valioesi <Vali_Schagerl@web.de>
2020-06-10 16:48:06 +0200

    Change date for signature history

--- src/hooks/api/getSignatureHistory/index.js
+++ src/hooks/api/getSignatureHistory/index.js
@@ -3,7 +3,7 @@ import { getRequest } from '../shared/requests';
 
 export const useSignatureHistory = () => {
   const [stats, setStats] = useState(() => {
-    getRequest('/analytics/signatures/history?date=2020-04-16').then(data =>
+    getRequest('/analytics/signatures/history?date=2020-04-15').then(data =>
       setStats(data.history)
     );
   });
