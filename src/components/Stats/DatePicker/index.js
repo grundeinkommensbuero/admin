23dd8151e8fb0f46f184432f736888127c213d69
Valioesi <Vali_Schagerl@web.de>
2020-08-06 13:01:01 +0200

    Reset history after clearing range

--- src/components/Stats/DatePicker/index.js
+++ src/components/Stats/DatePicker/index.js
@@ -3,17 +3,19 @@ import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 import './index.css';
 
-const DatePicker = ({ className, updateData }) => {
+const DatePicker = ({ updateData }) => {
   const [currentRange, setNewRange] = useState([]);
-  console.log({ currentRange });
 
   useEffect(() => {
     // Only update call function if start and end is set
-    if (currentRange.length === 2) {
+    if (currentRange && currentRange.length === 2) {
       updateData({
         startDate: currentRange[0].toISOString().substring(0, 10), // we only want to pass YYYY-MM-DD
         endDate: currentRange[1].toISOString().substring(0, 10),
       });
+    } else {
+      // Range was cleared --> update history without params
+      updateData({});
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentRange]);
