795709a602ae538fd2f42531090e8f316947ac12
Valioesi <Vali_Schagerl@web.de>
2021-02-02 11:50:03 +0100

    Moved config into json file to better use as module

--- src/campaignConfig.js
+++ src/campaignConfig.js
@@ -1,26 +1,8 @@
-const campaignConfig = {
-  campaigns: [
-    {
-      code: 'schleswig-holstein-1',
-      name: 'Schleswig Holstein 1',
-    },
-    {
-      code: 'brandenburg-1',
-      name: 'Brandenburg 1',
-    },
-    {
-      code: 'hamburg-1',
-      name: 'Hamburg 1',
-    },
-    {
-      code: 'berlin-1',
-      name: 'Berlin 1',
-    },
-    {
-      code: 'bremen-1',
-      name: 'Bremen 1',
-    },
-  ],
-};
+const campaigns = require('../campaigns.json');
+
+const campaignConfig = campaigns.map(({ name, round }) => ({
+  name: `${name} ${round}`,
+  code: `${name}-${round}`,
+}));
 
 export default campaignConfig;
