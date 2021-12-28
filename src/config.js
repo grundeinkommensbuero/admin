ef30a372cede7e4e06612eee536e0b4544c1c3db
Valioesi <Vali_Schagerl@web.de>
2021-12-28 18:08:21 +0100

    Use dev backend in deploy preview

--- src/config.js
+++ src/config.js
@@ -1,17 +1,19 @@
 console.log('env', process.env.NODE_ENV);
+const useDevBackend =
+  process.env.NODE_ENV === 'development' ||
+  process.env.GATSBY_USE_DEV_BACKEND === 'override';
+
 const config = {
   API: {
-    INVOKE_URL:
-      process.env.NODE_ENV === 'development'
-        ? 'https://2j0bcp5tr9.execute-api.eu-central-1.amazonaws.com/dev'
-        : 'https://ag5gu1z06h.execute-api.eu-central-1.amazonaws.com/prod',
+    INVOKE_URL: useDevBackend
+      ? 'https://2j0bcp5tr9.execute-api.eu-central-1.amazonaws.com/dev'
+      : 'https://ag5gu1z06h.execute-api.eu-central-1.amazonaws.com/prod',
   },
   COGNITO: {
     REGION: 'eu-central-1',
-    USER_POOL_ID:
-      process.env.NODE_ENV === 'development'
-        ? 'eu-central-1_R7ucfhvJM'
-        : 'eu-central-1_q89dI31Fh',
+    USER_POOL_ID: useDevBackend
+      ? 'eu-central-1_R7ucfhvJM'
+      : 'eu-central-1_q89dI31Fh',
     USER_POOL_CLIENT_ID: process.env.REACT_APP_COGNITO_CLIENT_ID,
   },
 };
